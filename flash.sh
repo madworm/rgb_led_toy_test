07ac6601240da1528677cf9d861bb0735ca76d5e
Robert Spitzenpfeil <robert@linuxbox.site>
2010-11-30 18:53:52 +0100

    Tweaks in flash.sh

--- flash.sh
+++ flash.sh
@@ -6,6 +6,6 @@
 # the bootloader is the typical arduino one and runs at 19200
 #
 
-avrdude -c usbtiny -p atmega168 -P usb -b 115200 -e -u -U lock:w:0x3f:m -U efuse:w:0x00:m -U hfuse:w:0xDD:m -U lfuse:w:0xE2:m
+avrdude -c usbtiny -p atmega168 -P usb -b 115200 -e -u -U lock:w:0x3f:m -U lfuse:w:0xE2:m -U hfuse:w:0xDD:m -U efuse:w:0x00:m
 avrdude -c usbtiny -p atmega168 -B 1 -P usb -b 115200 -U flash:w:rgb_led_toy_test__and__bootloader.ATmega168.8MHz.hex -U lock:w:0x0f:m
 
