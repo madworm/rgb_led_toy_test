3c07c5eb6080ee1ffe5b66efd4b5a9802209a958
Robert Spitzenpfeil <robert@linuxbox.site>
2011-06-12 04:10:19 +0200

    Added usbtiny/arduinoisp programmer selection to 'flash.sh'

--- flash.sh
+++ flash.sh
@@ -6,6 +6,26 @@
 # the bootloader is optiboot running at 115200
 #
 
-avrdude -c usbtiny -p atmega168 -P usb -b 115200 -e -B 100 -U lock:w:0x3f:m -U lfuse:w:0xE2:m -U hfuse:w:0xDD:m -U efuse:w:0x04:m
-avrdude -c usbtiny -p atmega168 -B 1 -P usb -b 115200 -U flash:w:rgb_led_toy_test__plus__optiboot_pro_8Mhz.hex -U lock:w:0x0f:m
+function flash_bootloader {
+  avrdude -c $PROGRAMMER -p atmega168 -B 100 -P $PORT -b $BAUDRATE -e -U lock:w:0x3F:m -U lfuse:w:0xE2:m -U hfuse:w:0xDD:m -U efuse:w:0x04:m
+  avrdude -c $PROGRAMMER -p atmega168 -B 1 -P $PORT -b $BAUDRATE -U flash:w:rgb_led_toy_test__plus__optiboot_pro_8Mhz.hex -U lock:w:0x0F:m
+}
 
+case $1 in
+  usbtiny)
+    BAUDRATE="115200"
+    PROGRAMMER=$1
+    PORT="usb"
+    flash_bootloader
+  ;;
+  arduinoisp)
+    BAUDRATE="19200"
+    PROGRAMMER="arduino"
+    PORT=${2:-/dev/ttyUSB0}
+    flash_bootloader
+  ;;
+  *)
+    echo -e  "\n usage: $0 usbtiny|arduinoisp port (default: /dev/ttyUSB0)
+              \n"
+  ;;
+esac
