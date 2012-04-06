35eec5427dbf8eb3d30967e184eb79d235509b59
Robert Spitzenpfeil <robert@linuxbox.site>
2012-04-06 18:47:51 +0200

    Reverted the V1 boards to LilyPadBOOT_168.hex + flash.sh adjustments.

--- V1_demo/flash.sh
+++ V1_demo/flash.sh
@@ -3,12 +3,12 @@
 #
 # if you get write errors increase the -B xx value
 #
-# the bootloader is optiboot running at 115200
+# the bootloader is LilyPadBOOT_168.hex running at 19200
 #
 
 function flash_bootloader {
-  avrdude -c $PROGRAMMER -p atmega168 -B 100 -P $PORT -b $BAUDRATE -e -U lock:w:0x3F:m -U lfuse:w:0xE2:m -U hfuse:w:0xDD:m -U efuse:w:0x04:m
-  avrdude -c $PROGRAMMER -p atmega168 -B 1 -P $PORT -b $BAUDRATE -U flash:w:V1_demo__plus__optiboot_pro_8Mhz.hex -U lock:w:0x0F:m
+  avrdude -c $PROGRAMMER -p atmega168 -B 100 -P $PORT -b $BAUDRATE -e -U lock:w:0x3F:m -U lfuse:w:0xE2:m -U hfuse:w:0xDD:m -U efuse:w:0x00:m
+  avrdude -c $PROGRAMMER -p atmega168 -B 1 -P $PORT -b $BAUDRATE -U flash:w:V1_demo__plus__LilyPadBOOT_168_8Mhz.hex -U lock:w:0x0F:m
 }
 
 case $1 in
