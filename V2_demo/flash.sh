b8a5cc1238ce94deb440435a4d7a6652e4f67d58
Robert Spitzenpfeil <robert@linuxbox.site>
2012-04-06 19:46:40 +0200

    Moved everything to 'ATmegaBOOT_168_pro_8MHz.hex' + necessary adaptions.

--- V2_demo/flash.sh
+++ V2_demo/flash.sh
@@ -3,11 +3,11 @@
 #
 # if you get write errors increase the -B xx value
 #
-# the bootloader is optiboot running at 115200
+# the bootloader is 'ATmegaBOOT_168_pro_8MHz.hex' running at 19200
 #
 
 function flash_bootloader {
-  avrdude -c $PROGRAMMER -p atmega168 -B 100 -P $PORT -b $BAUDRATE -e -U lock:w:0x3F:m -U lfuse:w:0xE2:m -U hfuse:w:0xDD:m -U efuse:w:0x04:m
+  avrdude -c $PROGRAMMER -p atmega168 -B 100 -P $PORT -b $BAUDRATE -e -U lock:w:0x3F:m -U lfuse:w:0xE2:m -U hfuse:w:0xDD:m -U efuse:w:0x00:m
   avrdude -c $PROGRAMMER -p atmega168 -B 1 -P $PORT -b $BAUDRATE -U flash:w:$HEXFILE:i -U lock:w:0x0F:m
 }
 
--- arduino-setup/readme.txt
+++ arduino-setup/readme.txt
@@ -2,7 +2,7 @@
 As the board runs with the internal RC oscillator (8MHz), which may be
 a bit unstable and off the target frequency, I recommend to use:
 
-'LilyPad' bootloader, which runs as 19200.
+'ATmegaBOOT_168_pro_8MHz.hex' bootloader, which runs as 19200.
 
-That is more reliable than using 'optiboot' at 115200.
+That is much more reliable than using 'optiboot' at 115200.
 
